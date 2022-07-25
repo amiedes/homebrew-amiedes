f321a505331668dcc610470e5c0cb1f6411e911a
Alberto Miedes Garcés <albertomg994@gmail.com>
2022-07-25 12:51:37 +0200

    Fix

--- Formula/gdal.rb
+++ Formula/gdal.rb
@@ -1,31 +1,38 @@
 class Gdal < Formula
-  desc "GDAL package with Geo Parquet support"
-  homepage ""
-  url "https://github.com/OSGeo/gdal/releases/download/v3.5.1/gdal-3.5.1.tar.gz"
-  sha256 "7c4406ca010dc8632703a0a326f39e9db25d9f1f6ebaaeca64a963e3fac123d1"
-  license ""
+  desc 'GDAL package with Geo Parquet support'
+  homepage ''
+  url 'https://github.com/OSGeo/gdal/releases/download/v3.5.1/gdal-3.5.1.tar.gz'
+  sha256 '7c4406ca010dc8632703a0a326f39e9db25d9f1f6ebaaeca64a963e3fac123d1'
+  license ''
 
-  depends_on "cmake" => :build
-  depends_on "openssl" => :build
-  depends_on "apache-arrow" => :build
+  depends_on 'cmake' => :build
+  depends_on 'openssl' => :build
+  depends_on 'apache-arrow' => :build
 
   def install
-    puts "Package installed"
+    puts 'Package installed'
     # ENV.deparallelize
-    # system "./configure", "--disable-debug",
-    #                       "--disable-dependency-tracking",
-    #                       "--disable-silent-rules",
-    #                       "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    # system "make", "install"
+    # system './configure', '--disable-debug',
+    #                       '--disable-dependency-tracking',
+    #                       '--disable-silent-rules',
+    #                       '--prefix=#{prefix}'
+    # system 'cmake', '.', *std_cmake_args
+    # system 'make', 'install'
+    system(
+      'cmake',
+      '-DOPENSSL_ROOT_DIR=/usr/local/opt/openssl',
+      '-DSQLite3_INCLUDE_DIR=/usr/local/opt/sqlite3/include',
+      '-DSQLITE3_LIBRARY=/usr/local/opt/sqlite3/lib/libsqlite3.dylib',
+      '..'
+    )
   end
 
   def uninstall
-    puts "Package uninstalled"
+    puts 'Package uninstalled'
   end
 
   test do
-    system "false"
+    system 'false'
   end
 end
 
@@ -36,9 +43,9 @@ end
 # mkdir build
 # cd build
 
-# echo 'export PATH="/usr/local/opt/sqlite3/bin:$PATH"' >> ~/.zshrc
-# export LDFLAGS="-L/usr/local/opt/sqlite3/lib"
-# export CPPFLAGS="-I/usr/local/opt/sqlite3/include"
+# echo 'export PATH='/usr/local/opt/sqlite3/bin:$PATH'' >> ~/.zshrc
+# export LDFLAGS='-L/usr/local/opt/sqlite3/lib'
+# export CPPFLAGS='-I/usr/local/opt/sqlite3/include'
 # cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DSQLite3_INCLUDE_DIR=/usr/local/opt/sqlite3/include -DSQLITE3_LIBRARY=/usr/local/opt/sqlite3/lib/libsqlite3.dylib ..
 # cmake --build .
 # cmake --build . --target install
